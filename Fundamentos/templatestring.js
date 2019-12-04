4afe983274ce097a60f06736e056c00ced821fea
Renan Tavares <renanntavares20@hotmail.com>
2019-12-04 15:49:42 -0300

    Aula de templetes

---
+++ Fundamentos/templatestring.js
@@ -0,0 +1,11 @@
+const nome = "Rebeca"
+const concatenacao = "Olá ".concat(nome) + "!"
+const template = `
+Olá
+${nome}!`
+console.log(concatenacao, template)
+
+const up = texto => texto.toUpperCase()
+console.log(`Eeeeei.... ${up("cuidado")}`)
+
+
