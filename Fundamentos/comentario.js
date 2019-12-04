5022b61fd6a1c6d79b59f3e344e1f1e511f2392f
Renan Tavares <renanntavares20@hotmail.com>
2019-12-04 10:47:15 -0300

    first commit

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,7 @@
+// comentário de uma linha
+console.log("Linha1")
+
+/* comentário
+de múltiplas linhas
+*/
+console.log('Linha 3')
---
+++ Fundamentos/tipagemfraca.js
@@ -0,0 +1,7 @@
+let qualquer = "Legal"
+console.log(qualquer)
+console.log(typeof qualquer)
+
+legal = 3
+console.log(legal)
+console.log(typeof legal)
